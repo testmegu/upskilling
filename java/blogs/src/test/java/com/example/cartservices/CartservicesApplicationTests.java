666bc6f090b6fb7b6e5cf9eed025b5d7bd7bc7ab
K <meghana.a.k@capgemini.com>
2023-02-08 23:54:49 +0530

    testing added

---
+++ java/blogs/src/test/java/com/example/cartservices/CartservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cartservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
