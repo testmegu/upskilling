d1badcf883e9d83f200d91c6f1b66d9f7397e833
K <meghana.a.k@capgemini.com>
2023-02-08 23:56:51 +0530

    testing added

---
+++ java/discover-server/src/test/java/com/example/discoverserver/DiscoverServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.discoverserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoverServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
